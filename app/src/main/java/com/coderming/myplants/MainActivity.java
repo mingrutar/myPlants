1c50fe6660e3668231338f88446a4f3549f145db
ming <rutar.ming@gmail.com>
2016-04-16 01:21:53 -0700

    add camera

--- app/src/main/java/com/coderming/myplants/MainActivity.java
+++ app/src/main/java/com/coderming/myplants/MainActivity.java
@@ -1,15 +1,53 @@
 package com.coderming.myplants;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    private Uri fileUri;
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        String folderNsme = "my_plants";
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), folderNsme);
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        return mediaFile;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +57,46 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        File file = getOutputMediaFile(); // create a file to save the image
+        fileUri = Uri.fromFile(file);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+                // start the image capture Intent
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);            }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    Log.i(LOG_TAG, "Image saved to:" + data.getData());
+                    // Image captured and saved to fileUri specified in the Intent
+                    Toast.makeText(this, "Image saved to:\n" +
+                            data.getData(), Toast.LENGTH_LONG).show();
+                } else {
+                    File file = new File(fileUri.getPath());
+                    if (file.exists()) {
+                        Log.i(LOG_TAG, "data = null but image saved to:" + file.getName());
+                    } else {
+                        Log.w(LOG_TAG, "image not saved to:" + file.getName());
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.i(LOG_TAG, "User canceled image capturing");
+                // User cancelled the image capture
+            } else {
+                Log.i(LOG_TAG, String.format("Got error code in launching camera: %d", requestCode));
+                // Image capture failed, advise user
+            }
+        }
     }
 
     @Override
